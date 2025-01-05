29524c812368a727e9e89261621e58fba51186b2
Max Sarychau <max.sarychau@tulip.com>
2025-01-05 22:35:19 +0100

    Set bower-comps

--- bower_components/underscore/modules/isDataView.js
+++ bower_components/underscore/modules/isDataView.js
@@ -1,16 +1,14 @@
 import tagTester from './_tagTester.js';
 import isFunction from './isFunction.js';
 import isArrayBuffer from './isArrayBuffer.js';
-import { hasDataViewBug } from './_stringTagBug.js';
+import { hasStringTagBug } from './_stringTagBug.js';
 
 var isDataView = tagTester('DataView');
 
 // In IE 10 - Edge 13, we need a different heuristic
 // to determine whether an object is a `DataView`.
-// Also, in cases where the native `DataView` is
-// overridden we can't rely on the tag itself.
-function alternateIsDataView(obj) {
+function ie10IsDataView(obj) {
   return obj != null && isFunction(obj.getInt8) && isArrayBuffer(obj.buffer);
 }
 
-export default (hasDataViewBug ? alternateIsDataView : isDataView);
+export default (hasStringTagBug ? ie10IsDataView : isDataView);
